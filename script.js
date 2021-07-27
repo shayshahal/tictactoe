e9053d56ef68fb21b30fb1921a5660dbcd821b23
shayshahal <shay840@gmail.com>
2021-07-27 14:42:01 +0300

    redesign functionality of start/restart

--- script.js
+++ script.js
@@ -6,8 +6,26 @@ const gameBoard = (() =>
         [0 , 0 , 0],
         [0 , 0 , 0]
     ];
+    let startDiv ;
+    let restartDiv;
+    const showStart = () =>
+    {
+        startDiv = document.createElement("div");
+        startDiv.style.opacity = "0.5";
+        startDiv.style.backgroundColor = "White";
+        startDiv.textContent = "Press to START!";
+        startDiv.style.textAlign = "center";
+        startDiv.style.lineHeight = "35rem";
+        startDiv.style.fontSize = "1.5rem";
+        startDiv.style.userSelect = "none";
+        startDiv.style.gridRow = "1 / 4";
+        startDiv.style.gridColumn = "1 / 4";
+        startDiv.addEventListener("click", init);
+        gb.appendChild(startDiv);
+    }
     const init = () =>
     {     
+        gb.removeChild(startDiv)
         for (let i = 0; i < 9; i++) 
         {
             const newDiv = document.createElement("div");
@@ -16,6 +34,6 @@ const gameBoard = (() =>
             gb.appendChild(newDiv);
         }
     }
-    return {init};
+    return {showStart};
 })();
-gameBoard.init();
+gameBoard.showStart();
