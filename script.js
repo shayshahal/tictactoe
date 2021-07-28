e52823ef0dad6841310726c262441d60ee914658
shayshahal <shay840@gmail.com>
2021-07-28 23:42:09 +0300

    game functionality progress

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+const player = (type) =>
+{
+    
+    const move = (e, array, x, y) =>
+    {
+        if(type == 1)
+        {
+            e.textContent = 'X';
+        }
+        else
+        {
+            e.textContent = 'O';
+        }
+        array[x][y] = type;
+    }
+    return{move};
+}
 const gameBoard = (() =>
 {
     const gb = document.getElementById("gameboard")
-    const array = [
-        [0 , 0 , 0],
-        [0 , 0 , 0],
-        [0 , 0 , 0]
-    ];
     let startDiv ;
-    let restartDiv;
     const showStart = () =>
     {
         startDiv = document.createElement("div");
@@ -24,26 +35,46 @@ const gameBoard = (() =>
     }
     const init = () =>
     {     
+        const array = [
+            [0 , 0 , 0],
+            [0 , 0 , 0],
+            [0 , 0 , 0]
+        ];
         gb.removeChild(startDiv)
-        for (let i = 0; i < 9; i++) 
+        for (let x = 0; x < 3; x++) 
         {
-            const newDiv = document.createElement("div");
-            newDiv.setAttribute('style', 'background-color: White');    
-            newDiv.classList.add("game-box");
-            gb.appendChild(newDiv);
+            for (let y = 0; y < 3; y++) 
+            {
+                const newDiv = document.createElement("div");
+                newDiv.style.backgroundColor = "White";
+                newDiv.style.fontSize = "7rem";
+                newDiv.style.userSelect = "none";
+                newDiv.classList.add("game-box");
+                function makeMove()
+                {
+                    gameFlow.move(newDiv, array, x, y);
+                }
+                newDiv.addEventListener("click", makeMove);
+                gb.appendChild(newDiv);           
+            }
         }
     }
     return {showStart};
 })();
 
-const gameFlow = (() => 
+const gameFlow = ((player1, player2) => 
 {
     let xturn = true;
-    const isWon = (array)
-})();
-const player = (name, type) =>
-{
-    let score = 0;
+    const move = (e, array, x, y) => 
+    {
+        if(array[x][y] == 0)
+        {
+            xturn ? player1.move(e, array, x, y) : player2.move(e, array, x, y);
+            xturn = !xturn;
+            console.log(array);
+        }
+    }
+    return {move};
+})(player(1), player(2));
 
-}
 gameBoard.showStart();
